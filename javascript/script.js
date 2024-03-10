673f304f2e67b2a285e7dae6d7274d7cc80e9f74
David Fleming <tonage72@gmail.com>
2024-03-09 20:36:21 -0500

    Complete maybe.

--- javascript/script.js
+++ javascript/script.js
@@ -10,6 +10,57 @@ navDailyButton.addEventListener("click", displayDailyStats);
 navWeeklyButton.addEventListener("click", displayWeeklyStats);
 navMonthlyButton.addEventListener("click", displayMonthlyStats);
 
+const hoursDailyCurrent = document.querySelectorAll('.hours-daily-current');
+const hoursWeeklyCurrent = document.querySelectorAll('.hours-weekly-current');
+const hoursMonthlyCurrent = document.querySelectorAll('.hours-monthly-current');
+
+const hoursDailyPrevious = document.querySelectorAll('.hours-daily-previous');
+const hoursWeeklyPrevious = document.querySelectorAll('.hours-weekly-previous');
+const hoursMonthlyPrevious = document.querySelectorAll('.hours-monthly-previous');
+
+function fetchJSON(navSelection) {
+	fetch('../data.json')
+		.then(res => {
+			return res.json();
+		})
+		.then(data => {
+			switch (navSelection) {
+				case 'daily':
+					let d = 0;
+					while (d < data.length) {
+						hoursDailyCurrent[d].textContent = hourDetermination(data[d].timeframes.daily.current);
+						hoursDailyPrevious[d].textContent = "Previous - " + hourDetermination(data[d].timeframes.daily.previous);
+						d++;
+					}
+					break
+				case 'weekly':
+					let w = 0;
+					while (w < data.length) {
+						hoursWeeklyCurrent[w].textContent = hourDetermination(data[w].timeframes.weekly.current);
+						hoursWeeklyPrevious[w].textContent = "Previous - " + hourDetermination(data[w].timeframes.weekly.previous);
+						w++;
+					}
+					break
+				case 'monthly':
+					let m = 0;
+					while (m < data.length) {
+						hoursMonthlyCurrent[m].textContent = hourDetermination(data[m].timeframes.monthly.current);
+						hoursMonthlyPrevious[m].textContent = "Previous - " + hourDetermination(data[m].timeframes.monthly.previous);
+						m++;
+					}
+					break
+			}
+		})
+}
+
+function hourDetermination(hours) {
+	if (hours == 1) {
+		return hours + "hr";
+	} else {
+		return hours + "hrs";
+	}
+}
+
 function displayDailyStats() {
 
 	navDailyButton.style.color = "hsl(236, 100%, 87%)";
@@ -18,6 +69,9 @@ function displayDailyStats() {
 
 	statsDaily.forEach(span => {
 		span.style.display = "flex";
+
+		fetchJSON('daily');
+
 	});
 	statsWeekly.forEach(span => {
 		span.style.display = "none";
@@ -38,6 +92,9 @@ function displayWeeklyStats() {
 	});
 	statsWeekly.forEach(span => {
 		span.style.display = "flex";
+
+		fetchJSON('weekly');
+
 	});
 	statsMonthly.forEach(span => {
 		span.style.display = "none";
@@ -58,6 +115,9 @@ function displayMonthlyStats() {
 	});
 	statsMonthly.forEach(span => {
 		span.style.display = "flex";
+
+		fetchJSON('monthly');
+
 	});
 }
 
